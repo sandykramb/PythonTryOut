78c3712f9776c1ea154cf289795423bf7b8e93ac
Sandy kramb <sandykramb@gmail.com>
2017-05-12 17:41:42 -0300

    First commit

---
+++ Python.1st.steps.py
@@ -0,0 +1,42 @@
+#variables
+My variable = 10
+my_int = 7
+my_float = 1.23
+
+#boolean: True or False
+My_bool = True
+
+#Change the value of my_int to 3 on line 8 – reassigning.
+my_int = 7
+my_int = 3
+print my_int
+
+"""The # sign will only comment out a single line. While you could write a multi-line comment"""
+
+#Math
+addition = 72 + 23
+subtraction = 108 - 204
+multiplication = 108 * 0.5
+division = 108 / 9
+#exponets
+eggs = 10 ** 2
+
+#Modulo returns the remainder from a division. 
+Spam = 7 % 6
+
+
+#1st test on python
+monty = True
+python = 1.234
+monty_python = python ** 2
+
+#A String can contain letters, numbers, and symbols
+brian = "hello life!"
+
+#Python thinks the apostrophe ends the string, use the backslash
+“Isn\'t that good?”
+
+#Each character in a string is assigned a number. This number is called the index, start with 0
+#S A N D Y
+#0 1 2 3 4
+second_letter = “SANDY”[1]
